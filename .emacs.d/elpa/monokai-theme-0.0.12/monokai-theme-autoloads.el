5d086ff343d2cfb01ae6061be5d3765f593af855
gefracto <evgeniy.kostrika@gmail.com>
2017-08-01 00:55:05 +0300

    .emacs updated

---
+++ .emacs.d/elpa/monokai-theme-0.0.12/monokai-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; monokai-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "monokai-theme" "monokai-theme.el" (22866 10988
+;;;;;;  771859 902000))
+;;; Generated autoloads from monokai-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))) (when (not window-system) (custom-set-faces '(default ((t (:background nil)))))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; monokai-theme-autoloads.el ends here
